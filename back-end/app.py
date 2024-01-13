265a26c7036a493f1cf2dfede0bf3578cc0c03d9
Ri-Hong <riri.hong@gmail.com>
2024-01-13 17:01:24 -0500

    Scanning works now

--- back-end/app.py
+++ back-end/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from routes.scan import scan  # Importing the Blueprint
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Allow only specific origin
 
 app.register_blueprint(scan, url_prefix='/scan')  # Registering the Blueprint
 
--- back-end/routes/scan.py
+++ back-end/routes/scan.py
@@ -3,46 +3,55 @@ from flask import Blueprint, jsonify, request
 import http.client
 import typing
 import urllib.request
+import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Image
+
+import os
+from dotenv import load_dotenv
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../GACKey.json"
+
+load_dotenv()
+PROJECT_ID = os.getenv('PROJECT_ID')
+LOCATION = os.getenv('LOCATION')
+CODE_CHAT_MODEL = os.getenv('CODE_CHAT_MODEL')
+
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
 
 scan = Blueprint('scan', __name__)
 
-
-
 # create helper function
 def load_image_from_url(image_url: str) -> Image:
-    with urllib.request.urlopen(image_url) as response:
+    req = urllib.request.Request(
+        image_url, 
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    with urllib.request.urlopen(req) as response:
         response = typing.cast(http.client.HTTPResponse, response)
         image_bytes = response.read()
     return Image.from_bytes(image_bytes)
 
 
+@scan.route('/say-hi', methods=['GET'])
+def say_hi():
+    return jsonify({"message": "Hi"}), 200
+
 @scan.route('/convert-text', methods=['GET'])
 def convert_text():
+    # Load images from Cloud Storage URI
+    image = load_image_from_url(
+        "https://cdn.discordapp.com/attachments/1187776063284195393/1195838041667940372/sample_prescription.png?ex=65b571f6&is=65a2fcf6&hm=08daa708feb1170befdacaeb1d54054c6ee2f983dae57a396e640c4d29d67c41&"
+    )
 
-# Load images from Cloud Storage URI
-landmark1 = load_image_from_url(
-    "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark1.png"
-)
-landmark2 = load_image_from_url(
-    "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark2.png"
-)
-landmark3 = load_image_from_url(
-    "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark3.png"
-)
+    # Pass multimodal prompt
+    model = GenerativeModel(CODE_CHAT_MODEL)
+    response = model.generate_content(
+        [
+            image,
+            "What is the prescription?"
+        ]
+    )
+    print(response)
 
-
-# Pass multimodal prompt
-model = GenerativeModel("gemini-pro-vision")
-response = model.generate_content(
-    [
-        landmark1,
-        "city: Rome, Landmark: the Colosseum",
-        landmark2,
-        "city: Beijing, Landmark: Forbidden City",
-        landmark3,
-    ]
-)
-print(response)
-
-    return jsonify({"response": "HELLO"}), 200
+    return jsonify({"response": response.text}), 200
